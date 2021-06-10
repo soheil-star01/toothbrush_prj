fd6fc0424891cbaed3f1e244ebb82703537b2f1e
soheil-star01 <soheildolat@yahoo.com>
2021-06-10 10:53:15 +0300

    mac os supported. baudrate modified

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import sys
 import serial.tools.list_ports as port_list
 from pathlib import Path
 from datetime import datetime
@@ -17,14 +18,25 @@ while True:
 
 print(f"Device found on ", new_dev[0].name)
 
-ser = serial.Serial(new_dev[0].name, 9600)
+os_name = sys.platform
+os_valid = True
+if os_name == 'darwin':
+    print('Mac OS')
+    ser = serial.Serial('/dev/' + new_dev[0].name, 115200)
+elif 'win' in os_name:
+    print('Windows OS')
+    ser = serial.Serial(new_dev[0].name, 115200)
+else:
+    os_valid = False
+    print('OS is not supported!!')
+
 FLAG1 = True
 file_name = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-while True:
+while os_valid:
     s = ser.readline()
     with open(f'senLog_{file_name}.txt', 'ab') as fb:
         fb.write(s)
     if FLAG1:
-        print('Data is capturing....')
+        print('Data is capturing....\nPress Ctrl+C to exit')
         FLAG1 = False
     # print(s)
